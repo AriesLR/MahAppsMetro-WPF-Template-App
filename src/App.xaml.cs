2d314f6f86978359bbd38ba86ad01121594c6199
Bobjobob24 <bobsgaming24@gmail.com>
2026-06-12 20:34:13 -0500

    Update App.xaml.cs

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using Application = System.Windows.Application;
 
 namespace Metro_WPF_Template_App
 {
@@ -10,5 +8,4 @@ namespace Metro_WPF_Template_App
     public partial class App : Application
     {
     }
-
 }
