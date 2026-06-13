c4bf17e37418f854cb307747a6e40e5258926b60
Bobjobob24 <bobsgaming24@gmail.com>
2026-06-12 20:34:23 -0500

    Update AppConfig.cs

--- src/Resources/Config/AppConfig.cs
+++ src/Resources/Config/AppConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Diagnostics;
+using System.IO;
 using System.Reflection;
 
 namespace Metro_WPF_Template_App.Resources.Config
@@ -6,7 +7,9 @@ namespace Metro_WPF_Template_App.Resources.Config
     public class AppConfig
     {
         // App Version
-        public static string AppVersion => $"v{Assembly.GetExecutingAssembly().GetName().Version}";
+        public static string TitleAppVersion => $"v{Assembly.GetExecutingAssembly().GetName().Version}";
+
+        public static readonly string AppVersion = FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).FileVersion ?? "0.0.0";
 
         // App Config Paths
         public static readonly string appConfigFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "AriesLR", "Metro_WPF_Template_App");
